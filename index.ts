4a118040952a010ee4f0eb59050c5408ef6c81fa
Jean-Claude Antonio <jcantonio@alineo.com>
2025-11-21 18:27:43 +0100

    init

---
+++ index.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from 'openapi/core/core/ApiError';
+export { CancelablePromise, CancelError } from 'openapi/core/core/CancelablePromise';
+export { OpenAPI } from 'openapi/core/core/OpenAPI';
+export type { OpenAPIConfig } from 'openapi/core/core/OpenAPI';
+
+export type { Job } from './models/Job';
+export type { JobAuditLog } from './models/JobAuditLog';
+export type { NewJob } from './models/NewJob';
+export type { RegisteredJob } from './models/RegisteredJob';
+
+export { DefaultService } from './services/DefaultService';
